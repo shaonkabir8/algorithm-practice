2485c527dc4bbed2f732f31e99a704e79e72b81c
Taijul Islam <taijulislam.st9@gmail.com>
2020-04-30 15:13:43 +0600

    Adding a new file for practicing Es6 set method from freecodecamp

---
+++ Data Structures/SetsInEs6.js
@@ -0,0 +1,49 @@
+function checkSet1() {
+  var set = new Set([1, 2, 3, 3, 2, 1, 2, 3, 1]);
+  // change code below this line
+  set.add("Taco");
+  set.add("Cat");
+  set.add("Awesome");
+
+  // change code above this line
+  console.log(Array.from(set));
+  return set;
+}
+
+checkSet1();
+
+/* Going for the next challange */
+
+function checkSet2() {
+  var set = new Set([1, 2, 3, 4, 5]); //Create a set with values 1, 2, 3, 4, & 5
+
+  //Remove the value 2
+  set.delete(2);
+  //Remove the value 5
+  set.delete(5);
+  //Return the set
+  return set;
+}
+
+
+
+/* Going for the next challange */
+
+
+
+function checkSet(arrToBeSet, checkValue){
+
+    // change code below this line
+    const set = new Set(arrToBeSet);
+
+    const hasValue = set.has(checkValue);
+    const howBig = set.size;
+
+    return [hasValue, howBig]
+ 
+    // change code above this line
+ 
+ }
+ 
+ checkSet([ 1, 2, 3], 2); // Should return [ true, 3 ]
+ 
