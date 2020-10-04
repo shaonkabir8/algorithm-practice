f61d99e207d8fb24fece25f4c97f60639a706b0e
shaonkabir8 <shaonkabir98@gmail.com>
2020-10-04 14:55:54 +0600

    refactor: add [reverse a string] algorithm

---
+++ basic-algorithms/reverse-a-string.js
@@ -0,0 +1,35 @@
+/**********************
+	Reverse a String
+	Target:- We'll write a function which takes a string as it's parameter and return it's reverse.
+	
+	// we'll go through the steps:-
+	1. Convert the string into array [Because Array has a method to reverse item]
+	2. Reverse the Array Item
+	3. convert Array into String 
+***********************/
+const reverseString = (string) =>
+  return string.split('').reverse().join('');
+};
+
+const result_one = reverseString("Bangladesh");
+console.log(result_one); // Should return "hsedalgnaB"
+
+
+// Another Way
+// 1. Create a empty string variable
+// 2. Loop through the string from the oposide using (--) and store them to our Created Empty sting variable;
+
+function reverseString_with_loop(string) {
+
+  let stringContainer = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    stringContainer += string[i];
+  }
+
+  return stringContainer;
+}
+
+
+const result_two = reverseString_with_loop("Bangladesh");
+console.log(result_two)
